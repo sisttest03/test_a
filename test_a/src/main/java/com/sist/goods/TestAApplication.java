be4506f9d1e181e96cf2d05982027e0c439d1ee4
sisttest03 <sisttest03@gmail.com>
2021-03-16 16:21:55 +0900

    프로젝트 생성[김은영]

---
+++ test_a/src/main/java/com/sist/goods/TestAApplication.java
@@ -0,0 +1,13 @@
+package com.sist.goods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestAApplication.class, args);
+	}
+
+}
---
+++ test_a/src/test/java/com/sist/goods/TestAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sist.goods;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
